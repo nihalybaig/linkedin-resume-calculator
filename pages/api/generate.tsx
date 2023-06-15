92f97b873366901a5d352717983a68347f3ff9d1
abhisai <abi.sai@adaptiveai.ai>
2023-06-15 18:16:35 +0530

    Fixed tailwind issue and styled

--- pages/api/generate.tsx
+++ pages/api/generate.tsx
@@ -1,23 +1,30 @@
 export default function handler(req: any, res: any) {
-    // Process the request and generate the JSON response
-    const response = {
-        "experience_comments": "Based on your experience of 1.5 years, you have very little experience compared to the 10+ years needed for the job. Your score is 1 out of 10.",
-        "experience_match_score": 1,
-        "experience_points": "You need at least 8.5 more years of experience to improve your chances of being selected for this job.",
-        "skills_comments": "There is very little overlap between your skills and the required skills for the job. Your skills are more focused on technical areas such as Machine Learning, Data Engineering, Cloud Computing, and Search Engine Optimization, whereas the required skills are more focused on finance and data analysis tools. You may want to highlight any experience you have with SQL, Python, or financial reporting to show some familiarity with the required skills.",
-        "skills_match_score": 2,
-        "skills_points": "Consider learning some of the data analysis tools listed such as Tableau, SAS, and Alteryx. Additionally, highlighting any experience in corporate finance or loan/deposit pricing may also improve your chances.",
-        "summary_comments": "There is some overlap between your work experience and the job description, but there are significant differences as well. While you have experience in end-to-end machine learning solutions, the job is focused on financial analysis and requires skills in Excel, Tableau, and automation tools. Additionally, you should have knowledge in retail banking and products and at least one of the team's key treasury and corporate finance content areas. ",
-        "summary_match_score": 3,
-        "summary_points": "To better align your resume with this job description, you should highlight any experience you have with financial analysis and tools like Excel, Tableau, and Alteryx. It would also be helpful to emphasize your knowledge of retail banking and products. Consider adding specific examples of your experience in these areas.",
-        "title_comments": "Based on the information provided, your previous role as a Senior Data Scientist has some transferable skills to Treasury Analytics. However, the job titles are not an exact match and it seems that you may not have direct experience in this specific role.",
-        "title_match_score": 2,
-        "title_points": [
-            "Emphasize transferable skills in your resume that are relevant to the Treasury Analytics role",
-            "Highlight any experience you may have with financial data analysis",
-            "Consider taking relevant courses or certifications to strengthen your qualifications for this role"
-        ]
-    };
+  // Process the request and generate the JSON response
+  const response = {
+    experience_comments:
+      'Based on your experience of 1.5 years, you have very little experience compared to the 10+ years needed for the job. Your score is 1 out of 10.',
+    experience_match_score: 9,
+    experience_points:
+      'You need at least 8.5 more years of experience to improve your chances of being selected for this job.',
+    skills_comments:
+      'There is very little overlap between your skills and the required skills for the job. Your skills are more focused on technical areas such as Machine Learning, Data Engineering, Cloud Computing, and Search Engine Optimization, whereas the required skills are more focused on finance and data analysis tools. You may want to highlight any experience you have with SQL, Python, or financial reporting to show some familiarity with the required skills.',
+    skills_match_score: 2,
+    skills_points:
+      'Consider learning some of the data analysis tools listed such as Tableau, SAS, and Alteryx. Additionally, highlighting any experience in corporate finance or loan/deposit pricing may also improve your chances.',
+    summary_comments:
+      "There is some overlap between your work experience and the job description, but there are significant differences as well. While you have experience in end-to-end machine learning solutions, the job is focused on financial analysis and requires skills in Excel, Tableau, and automation tools. Additionally, you should have knowledge in retail banking and products and at least one of the team's key treasury and corporate finance content areas. ",
+    summary_match_score: 3,
+    summary_points:
+      'To better align your resume with this job description, you should highlight any experience you have with financial analysis and tools like Excel, Tableau, and Alteryx. It would also be helpful to emphasize your knowledge of retail banking and products. Consider adding specific examples of your experience in these areas.',
+    title_comments:
+      'Based on the information provided, your previous role as a Senior Data Scientist has some transferable skills to Treasury Analytics. However, the job titles are not an exact match and it seems that you may not have direct experience in this specific role.',
+    title_match_score: 7,
+    title_points: [
+      'Emphasize transferable skills in your resume that are relevant to the Treasury Analytics role',
+      'Highlight any experience you may have with financial data analysis',
+      'Consider taking relevant courses or certifications to strengthen your qualifications for this role',
+    ],
+  };
 
-    res.status(200).json(response);
+  res.status(200).json(response);
 }
--- pages/main.tsx
+++ pages/main.tsx
@@ -1,130 +1,143 @@
 import React, { useState } from 'react';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 export default function Main() {
-    const [file, setFile] = useState(null);
-    const [fileText, setFileText] = useState('');
-    const [text, setText] = useState('');
-    const [apiResponse, setApiResponse] = useState(null);
+  const [file, setFile] = useState(null);
+  const [fileText, setFileText] = useState('');
+  const [text, setText] = useState('');
+  const [apiResponse, setApiResponse] = useState(null);
 
-    const handleFileChange = (event: any) => {
-        setFile(event.target.files[0]);
-    };
+  const handleFileChange = (event: any) => {
+    setFile(event.target.files[0]);
+  };
 
-    const handleFileTextChange = (event: any) => {
-        setFileText(event.target.value);
-    };
+  const handleFileTextChange = (event: any) => {
+    setFileText(event.target.value);
+  };
 
-    const handleTextChange = (event: any) => {
-        setText(event.target.value);
-    };
+  const handleTextChange = (event: any) => {
+    setText(event.target.value);
+  };
 
-    const handleGenerate = async () => {
-        // Call your API here and set the response in the state variable `apiResponse`
-        // For example:
-        const response = await fetch('/api/generate', {
-            method: 'POST',
-            body: JSON.stringify({ text }),
-        });
-        const data = await response.json();
-        setApiResponse(data);
-    };
+  const handleGenerate = async () => {
+    // Call your API here and set the response in the state variable `apiResponse`
+    // For example:
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      body: JSON.stringify({ text }),
+    });
+    const data = await response.json();
+    setApiResponse(data);
+  };
 
-    return (
-        <div className="p-4" >
-            <h1 className="text-3xl font-bold mb-4" > Next.js Webpage </h1>
-
-            {/* < div className="mb-4" >
+  return (
+    <div className="p-4 mt-10">
+      {/* < div className="mb-4" >
                 <label className="block mb-2 font-bold" > Upload File </label>
                 < input type="file" onChange={handleFileChange} />
             </div> */}
-
-            < div className="mb-4" >
-                <label className="block mb-2 font-bold" > Input Resume Text </label>
-                < textarea
-                    className="w-full p-2 border border-gray-300 rounded"
-                    rows={5}
-                    value={text}
-                    onChange={handleTextChange}
-                />
-            </div>
-
-            < div className="mb-4" >
-                <label className="block mb-2 font-bold" > Input Long Text </label>
-                < textarea
-                    className="w-full p-2 border border-gray-300 rounded"
-                    rows={5}
-                    value={text}
-                    onChange={handleTextChange}
-                />
-            </div>
-
-            < button
-                className="px-4 py-2 bg-blue-500 text-white font-bold rounded"
-                onClick={handleGenerate}
-            >
-                Generate
-            </button>
-
-            {apiResponse && <ApiResponseTable apiResponse={apiResponse} />}
+      <div className="grid grid-cols-2">
+        <div className="mb-4 flex flex-col justify-center items-center">
+          <label className="block mb-2 font-bold "> Input Resume Text </label>
+          <textarea
+            className="w-1/2 p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:border-blue-500"
+            rows={5}
+            value={text}
+            onChange={handleTextChange}
+          />
         </div>
-    );
+        <div className="mb-4 flex flex-col justify-center items-center">
+          <label className="block mb-2 font-bold">
+            {' '}
+            Input Job Description{' '}
+          </label>
+          <textarea
+            className="w-1/2 p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:border-blue-500"
+            rows={5}
+            value={text}
+            onChange={handleTextChange}
+          />
+        </div>
+      </div>
+      <div className="flex justify-center mt-16">
+        <button
+          className="p-3 bg-blue-500 text-white font-bold rounded"
+          onClick={handleGenerate}
+        >
+          Score My Resume
+        </button>
+      </div>
+
+      {apiResponse && <ApiResponseTable apiResponse={apiResponse} />}
+    </div>
+  );
 }
 
 function ApiResponseTable({ apiResponse }: any) {
-    const sectionKeys = [
-        { key: 'title', label: 'Title' },
-        { key: 'experience', label: 'Experience' },
-        { key: 'skills', label: 'Skills' },
-        { key: 'summary', label: 'Summary' },
-    ];
+  const sectionKeys = [
+    { key: 'title', label: 'Title' },
+    { key: 'experience', label: 'Experience' },
+    { key: 'skills', label: 'Skills' },
+    { key: 'summary', label: 'Summary' },
+  ];
 
-    return (
-        <div className="mt-4">
-            <h2 className="text-2xl font-bold mb-4">API Response:</h2>
-            <div className="shadow overflow-x-auto border border-gray-300">
-                <table className="min-w-full bg-white border border-gray-300">
-                    <thead>
-                        <tr>
-                            <th className="py-3 px-4 border-b border-gray-300 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Section
-                            </th>
-                            <th className="py-3 px-4 border-b border-gray-300 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Score
-                            </th>
-                            <th className="py-3 px-4 border-b border-gray-300 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Comments
-                            </th>
-                            <th className="py-3 px-4 border-b border-gray-300 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Recommendation
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {sectionKeys.map(({ key, label }) => (
-                            <tr key={key}>
-                                <td className="py-4 px-6 border-b border-gray-300 whitespace-nowrap">
-                                    {label}
-                                </td>
-                                <td className="py-4 px-6 border-b border-gray-300 whitespace-nowrap">
-                                    {apiResponse[`${key}_match_score`]}
-                                </td>
-                                <td className="py-4 px-6 border-b border-gray-300">
-                                    {apiResponse[`${key}_comments`]}
-                                </td>
-                                <td className="py-4 px-6 border-b border-gray-300">
-                                    {Array.isArray(apiResponse[`${key}_points`])
-                                        ? apiResponse[`${key}_points`].map((point: any, index: any) => (
-                                            <p key={index} className="mb-2">
-                                                {point}
-                                            </p>
-                                        ))
-                                        : apiResponse[`${key}_points`]}
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+  const getPathColor = (val: number) => {
+    if (val < 5) {
+      return '#FF0000';
+    } else if (val < 9) {
+      return '#FF8C00';
+    } else {
+      return '#2E8B57';
+    }
+  };
+
+  return (
+    <div className="mt-4">
+      <h2 className="text-2xl font-bold mb-4">Recommendations:</h2>
+      <div className="shadow overflow-x-auto border border-gray-300">
+        {sectionKeys.map(({ key, label }) => (
+          <div className="border-b border-gray-300 flex" key={key}>
+            <div className="flex flex-col justify-center items-center w-1/6">
+              <div className="py-4 px-6 border-gray-300 whitespace-nowrap font-bold">
+                {label}
+              </div>
+              <div className="py-4 px-6 border-gray-300 whitespace-nowrap">
+                <CircularProgressbar
+                  value={apiResponse[`${key}_match_score`] * 10}
+                  text={`${apiResponse[`${key}_match_score`] * 10}%`}
+                  className="h-20 w-20"
+                  styles={buildStyles({
+                    pathColor: getPathColor(apiResponse[`${key}_match_score`]),
+                  })}
+                />
+              </div>
             </div>
-        </div>
-    );
+            <div className="flex flex-col w-5/6">
+              <div className="flex items-center border-b">
+                <div className="font-semibold">Comments:</div>
+                <div className="py-4 px-6 border-gray-300">
+                  {apiResponse[`${key}_comments`]}
+                </div>
+              </div>
+              <div className="flex items-center">
+                <div className="font-semibold">Title Points:</div>
+                <div className="py-4 px-6">
+                  {Array.isArray(apiResponse[`${key}_points`])
+                    ? apiResponse[`${key}_points`].map(
+                        (point: any, index: any) => (
+                          <p key={index} className="mb-2">
+                            {point}
+                          </p>
+                        )
+                      )
+                    : apiResponse[`${key}_points`]}
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
